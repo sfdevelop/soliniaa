513461d258a1d2807132edbb3349d4717f497297
bonchbrubonch <58067420+bonchbrubonch@users.noreply.github.com>
2025-08-28 21:06:03 +0200

    add remarks

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,7 @@ $(function () {
 		min: 500,
 		max: 10000,
 		from: 4100,
+		postfix: "$"
 	});
 });
 
@@ -18,12 +19,23 @@ document.addEventListener("DOMContentLoaded", () => {
 	const menuBtn = document.querySelector(".header__menu-btn");
 	const box = document.querySelector(".header__box");
 	const boxClose = document.querySelector(".header__box-close");
-
+	const body = document.body;
+	const links = box.querySelectorAll("a");
 	menuBtn.addEventListener("click", () => {
 		box.classList.add("open");
+		body.classList.add("dark-bg");
 	});
 
 	boxClose.addEventListener("click", () => {
 		box.classList.remove("open");
+		body.classList.remove("dark-bg");
+	});
+
+	links.forEach(link => {
+		link.addEventListener("click", () => {
+			box.classList.remove("open");
+			body.classList.remove("dark-bg");
+		});
 	});
 });
+
